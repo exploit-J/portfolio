1fb4280fc01b0d01acd1830d0412fe1b590c8f56
exploit-j <kkarismajh@gmail.com>
2022-05-25 16:05:11 +0900

    header event JS 적용

--- js/header.js
+++ js/header.js
@@ -2,14 +2,18 @@ const headerTopBar = document.querySelector(".top-bar");
 const headerLeftBar = document.querySelector(".left-bar");
 const headerRightBar = document.querySelector(".right-bar");
 const headerBottomBar = document.querySelector(".bottom-bar");
-const nav = document.querySelector("nav");
+const nav = document.querySelector(".nav-pc");
 const hederTitle = document.querySelector("#header-title");
 const sideBar = document.querySelector(".nav-mo");
-const sideBarItem = document.querySelector(".nav-mo ul");
+const sideBarUl = document.querySelector(".nav-mo ul");
 const sideBarButton = document.querySelector(".sidebar-button");
 const bottomArrow = document.querySelectorAll(".bottom-arrow i");
-console.log(bottomArrow[0]);
+const toTopButton = document.querySelector(".top-button");
+const sideBarLi = document.querySelectorAll(".nav-mo-wrap li");
+const mainSectionTitle = document.querySelectorAll("main h2");
+const footer = document.querySelector("footer");
 
+// 페이지랜딩 이벤트
 window.onload = () => {
   setTimeout(() => {
     headerTopBar.style.animation = "topAni 3s";
@@ -25,7 +29,8 @@ window.onload = () => {
   }, 2300);
   setTimeout(() => {
     nav.style.animation = "nav-move 2s forwards";
-  }, 8000);
+    sideBar.style.animation = "sidebar-drop 1s forwards";
+  }, 1000);
   setTimeout(() => {
     bottomArrow[0].style.animation = "arrow 1s infinite";
     bottomArrow[1].style.animation = "arrow 1s 0.1s infinite";
@@ -36,15 +41,45 @@ window.onload = () => {
   }, 8000);
 };
 
-//sdie-bar on/off
+// sdie-bar on/off
 sideBarButton.addEventListener("click", () => {
   sideBar.classList.toggle("active");
-  sideBarItem.classList.toggle("active");
+  sideBarUl.classList.toggle("active");
   sideBarButton.classList.toggle("active");
 });
 
+//TypeIt
 new TypeIt("#header-title", { startDelay: 3000 })
   .type("안녕하세요,", { delay: 1000 })
   .type("<br>신입 프론트엔드 개발자", { delay: 500 })
   .type("<br>정 훈 입니다.", { delay: 100 })
   .go();
+
+// move to top button
+const topButtonActive = () => {
+  if (window.scrollY >= 600) {
+    toTopButton.classList.add("active");
+  } else if (window.scrollY < 600) {
+    toTopButton.classList.remove("active");
+  }
+};
+
+window.addEventListener("scroll", _.throttle(topButtonActive, 300));
+
+toTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+// move to section
+sideBarLi[0].addEventListener("click", () => {
+  mainSectionTitle[0].scrollIntoView({ behavior: "smooth", block: "start" });
+});
+sideBarLi[1].addEventListener("click", () => {
+  mainSectionTitle[1].scrollIntoView({ behavior: "smooth", block: "start" });
+});
+sideBarLi[2].addEventListener("click", () => {
+  mainSectionTitle[2].scrollIntoView({ behavior: "smooth", block: "start" });
+});
+sideBarLi[3].addEventListener("click", () => {
+  footer.scrollIntoView({ behavior: "smooth", block: "start" });
+});
---
+++ js/main.js
@@ -0,0 +1,18 @@
+// const aboutImage = document.querySelector(".about .img");
+// const aboutIntroduce = document.querySelector(".introduce");
+// const titleBorder = document.querySelectorAll("main h2");
+
+// const topButtonActive = () => {
+//     if (window.scrollY >= 600) {
+//       toTopButton.classList.add("active");
+//     } else if (window.scrollY < 600) {
+//       toTopButton.classList.remove("active");
+//     }
+
+//     if (window.scrollY == 800) {
+
+//     }
+//     console.log(window.scrollY);
+//   };
+
+//   window.addEventListener("scroll", _.throttle(topButtonActive, 300));
