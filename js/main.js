ef0c5b993fd5d63febdfae71c974bca06410e0c9
exploit-j <kkarismajh@gmail.com>
2022-05-26 11:18:24 +0900

    pf section js이벤트 적용

--- js/main.js
+++ js/main.js
@@ -1,29 +1,84 @@
-const aboutImage = document.querySelector(".about .img");
-const aboutIntroduce = document.querySelector(".introduce");
+const toTopButton_PC = document.querySelector(".top-button-pc");
 const sectionTitle = document.querySelectorAll("main h2");
 const titleBorder = document.querySelectorAll(".title-border");
 const header = document.querySelector("header");
 const aboutSection = document.querySelector(".about-wrap");
-const toTopButton_PC = document.querySelector(".top-button-pc");
+const aboutImage = document.querySelector(".about .img");
+const aboutIntroduce = document.querySelector(".introduce");
+const skillSection = document.querySelector(".skill");
 const skillIcon = document.querySelector(".icon-wrap");
 const skillDesc = document.querySelector(".skill-desc");
+const portfolioItem = document.querySelector(".pf-wrapper");
 
 // section appear event
 const sectionEvent = () => {
-  // console.log(window.scrollY);
-  // console.log("about Y", header.offsetHeight * 0.8);
-  // console.log("skill Y", header.offsetHeight + aboutSection.offsetHeight * 0.8);
+  const skillSectionAni = () => {
+    sectionTitle[1].style.animation = "title-appear 2s forwards";
+    titleBorder[1].style.animation = "border-scale 1s forwards";
+    skillIcon.style.animation = "side-move 1s forwards";
+    skillDesc.style.animation = "side-move 1s forwards";
+  };
+
+  const portfolioSectionAni = () => {
+    sectionTitle[2].style.animation = "title-appear 2s forwards";
+    titleBorder[2].style.animation = "border-scale 1s forwards";
+    portfolioItem.style.animation = "top-move 1s forwards";
+  };
+  console.log(window.scrollY);
+
+  console.log("about Y", header.offsetHeight * 0.8);
+  console.log(
+    "skill Y",
+    header.offsetHeight + aboutSection.offsetHeight * 0.85
+  );
+  console.log(
+    "portfolio Y",
+    header.offsetHeight +
+      aboutSection.offsetHeight +
+      skillSection.offsetHeight * 0.9
+  );
+
   if (window.scrollY > header.offsetHeight * 0.8) {
     sectionTitle[0].style.animation = "title-appear 2s forwards";
     titleBorder[0].style.animation = "border-scale 1s forwards";
     aboutImage.style.animation = "side-move 1s forwards";
     aboutIntroduce.style.animation = "side-move 1s forwards";
   }
-  if (window.scrollY >= header.offsetHeight + aboutSection.offsetHeight * 0.8) {
-    sectionTitle[1].style.animation = "title-appear 2s forwards";
-    titleBorder[1].style.animation = "border-scale 1s forwards";
-    skillIcon.style.animation = "side-move 1s forwards";
-    skillDesc.style.animation = "side-move 1s forwards";
+
+  if (window.innerWidth > 1024) {
+    if (
+      window.scrollY >=
+      header.offsetHeight + aboutSection.offsetHeight * 1.1
+    ) {
+      skillSectionAni();
+    }
+  } else if (window.innerWidth <= 1024) {
+    if (
+      window.scrollY >=
+      header.offsetHeight + aboutSection.offsetHeight * 0.85
+    ) {
+      skillSectionAni();
+    }
+  }
+
+  if (window.innerWidth > 1024) {
+    if (
+      window.scrollY >=
+      header.offsetHeight +
+        aboutSection.offsetHeight +
+        skillSection.offsetHeight * 0.9
+    ) {
+      portfolioSectionAni();
+    }
+  } else if (window.innerWidth <= 1024) {
+    if (
+      window.scrollY >=
+      header.offsetHeight +
+        aboutSection.offsetHeight +
+        skillSection.offsetHeight * 0.7
+    ) {
+      portfolioSectionAni();
+    }
   }
 };
 window.addEventListener("scroll", _.throttle(sectionEvent, 300));
