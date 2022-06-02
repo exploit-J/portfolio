9fafd3956eed0f16a209a6f3869d930a05198c21
exploit-j <kkarismajh@gmail.com>
2022-06-02 09:10:44 +0900

    main.js 코드 리팩토링

--- js/main.js
+++ js/main.js
@@ -2,75 +2,53 @@ const toTopButton_PC = document.querySelector(".top-button-pc");
 const sectionTitle = document.querySelectorAll("main h2");
 const titleBorder = document.querySelectorAll(".title-border");
 const header = document.querySelector("header");
-const aboutSection = document.querySelector(".about-wrap");
-const aboutImage = document.querySelector(".about .img");
-const aboutIntroduce = document.querySelector(".introduce");
-const skillSection = document.querySelector(".skill");
-const skillIcon = document.querySelector(".icon-wrap");
-const skillDesc = document.querySelector(".skill-desc");
-const portfolioItem = document.querySelectorAll(".pf-wrapper");
+const aboutSection = Array.from(document.querySelector(".about-wrap").children);
+const skillSection = Array.from(
+  document.querySelector(".skill .desc").children
+);
+const portfolioSection = document.querySelectorAll(".pf-wrapper");
+const portfolioItem = document.querySelectorAll(".portfolio.mo .pf-item");
+console.log(portfolioItem);
 
 // section appear event
 const sectionEvent = () => {
-  const skillSectionAni = () => {
-    sectionTitle[1].style.animation = "title-appear 2s forwards";
-    titleBorder[1].style.animation = "border-scale 1s forwards";
-    skillIcon.style.animation = "side-move 1s forwards";
-    skillDesc.style.animation = "side-move 1s forwards";
-  };
-
-  const portfolioSectionAni = () => {
-    sectionTitle[2].style.animation = "title-appear 2s forwards";
-    sectionTitle[3].style.animation = "title-appear 2s forwards";
-    titleBorder[2].style.animation = "border-scale 1s forwards";
-    titleBorder[3].style.animation = "border-scale 1s forwards";
-    portfolioItem.forEach((item) => {
-      item.style.animation = "top-move 1s forwards";
-    });
-  };
-
-  if (window.scrollY > header.offsetHeight * 0.8) {
-    sectionTitle[0].style.animation = "title-appear 2s forwards";
-    titleBorder[0].style.animation = "border-scale 1s forwards";
-    aboutImage.style.animation = "side-move 1s forwards";
-    aboutIntroduce.style.animation = "side-move 1s forwards";
-  }
-
-  if (window.innerWidth > 1024) {
-    if (
-      window.scrollY >=
-      header.offsetHeight + aboutSection.offsetHeight * 1.1
-    ) {
-      skillSectionAni();
+  let viewHeight = window.innerHeight;
+  sectionTitle.forEach((item) => {
+    const itemH = item.getBoundingClientRect().top;
+    if (viewHeight > itemH + 200) {
+      item.classList.add("active");
     }
-  } else if (window.innerWidth <= 1024) {
-    if (
-      window.scrollY >=
-      header.offsetHeight + aboutSection.offsetHeight * 0.85
-    ) {
-      skillSectionAni();
+  });
+  titleBorder.forEach((item) => {
+    const itemH = item.getBoundingClientRect().top;
+    if (viewHeight > itemH + 200) {
+      item.classList.add("active");
     }
-  }
-
-  if (window.innerWidth > 1024) {
-    if (
-      window.scrollY >=
-      header.offsetHeight +
-        aboutSection.offsetHeight +
-        skillSection.offsetHeight * 0.9
-    ) {
-      portfolioSectionAni();
+  });
+  aboutSection.forEach((item) => {
+    const itemH = item.getBoundingClientRect().top;
+    if (viewHeight > itemH + 200) {
+      item.classList.add("active");
     }
-  } else if (window.innerWidth <= 1024) {
-    if (
-      window.scrollY >=
-      header.offsetHeight +
-        aboutSection.offsetHeight +
-        skillSection.offsetHeight * 0.7
-    ) {
-      portfolioSectionAni();
+  });
+  skillSection.forEach((item) => {
+    const itemH = item.getBoundingClientRect().top;
+    if (viewHeight > itemH + 200) {
+      item.classList.add("active");
     }
-  }
+  });
+  portfolioSection.forEach((item) => {
+    const itemH = item.getBoundingClientRect().top;
+    if (viewHeight > itemH + 200) {
+      item.classList.add("active");
+    }
+  });
+  portfolioItem.forEach((item) => {
+    const itemH = item.getBoundingClientRect().top;
+    if (viewHeight > itemH + 200) {
+      item.classList.add("active");
+    }
+  });
 };
 window.addEventListener("scroll", _.throttle(sectionEvent, 300));
 
